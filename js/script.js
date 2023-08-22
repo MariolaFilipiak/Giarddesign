3b9b7ddd1de8b2b484560b98bfaf32af912aa1a4
Mariola Filipiak <mfilipiak61e@gmail.com>
2023-08-22 10:06:31 +0200

    macyjs gallery created

--- js/script.js
+++ js/script.js
@@ -1,42 +1,62 @@
 document.addEventListener("DOMContentLoaded", () => {
   const expandButton = document.getElementById("expandButton");
+  const galleryImages = document.querySelector(".gallery-images");
   const gradientOverlay = document.querySelector(".gradient-overlay");
-  const galleryImages = document.querySelectorAll(".gallery-img img");
   const modalImage = document.getElementById("modalImage");
   const modal = new bootstrap.Modal(document.getElementById("imageModal"));
 
-  const showImageInModal = (image) => {
-    modalImage.src = image.src;
+  const msnry = new Macy({
+    container: ".gallery-images",
+    mobileFirst: true,
+    columns: 1,
+    breakAt: { 576: 2, 768: 3, 992: 3 },
+    margin: { x: 40, y: 40 },
+  });
+
+  const newImages = [
+    "https://images.pexels.com/photos/11678318/pexels-photo-11678318.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/6829876/pexels-photo-6829876.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/788485/pexels-photo-788485.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/8838614/pexels-photo-8838614.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/1652966/pexels-photo-1652966.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/1065874/pexels-photo-1065874.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/1590336/pexels-photo-1590336.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/1607015/pexels-photo-1607015.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/327483/pexels-photo-327483.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  ];
+
+  const showImageInModal = (imageSrc) => {
+    modalImage.src = imageSrc;
     modal.show();
   };
 
+  const addImageClickListener = (image) => {
+    image.addEventListener("click", () =>
+      showImageInModal(image.getAttribute("src"))
+    );
+  };
+
   expandButton.addEventListener("click", () => {
-    expandButton.style.transform = "translateY(100%)";
+    expandButton.style.transform = "d-none";
     gradientOverlay.style.display = "none";
-    galleryImages.forEach((image) => {
-      image.style.display = "block";
-      image.addEventListener("click", () => showImageInModal(image));
-    });
 
-    const newImages = [
-      "https://images.pexels.com/photos/11678318/pexels-photo-11678318.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      "https://images.pexels.com/photos/6829876/pexels-photo-6829876.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      "https://images.pexels.com/photos/788485/pexels-photo-788485.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      "https://images.pexels.com/photos/8838614/pexels-photo-8838614.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      "https://images.pexels.com/photos/1652966/pexels-photo-1652966.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      "https://images.pexels.com/photos/1065874/pexels-photo-1065874.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    ];
+    galleryImages.querySelectorAll(".item").forEach(addImageClickListener);
 
-    const galleryRow = document.querySelector(".gallery-img.row");
-    newImages.forEach((imageSrc) => {
-      const newCol = document.createElement("div");
-      newCol.className = "col-lg-4 col-md-6 col-12 p-0";
-      const newImage = document.createElement("img");
-      newImage.src = imageSrc;
-      newImage.className = "w-100 img-fluid  mx-auto gallery-image";
-      newImage.addEventListener("click", () => showImageInModal(newImage));
-      newCol.appendChild(newImage);
-      galleryRow.appendChild(newCol);
-    });
+    const addImagesToGallery = (images) => {
+      images.forEach((imageUrl) => {
+        const newImage = new Image();
+        newImage.src = imageUrl;
+        newImage.classList.add("item");
+        newImage.alt = "image";
+        galleryImages.appendChild(newImage);
+        addImageClickListener(newImage);
+        newImage.addEventListener("load", () => msnry.recalculate(true));
+      });
+
+      msnry.recalculate(true);
+    };
+
+    addImagesToGallery(newImages);
+    expandButton.classList.add("d-none");
   });
 });
